5e97bb6f5ac43378711ac6f6506f3e53a753e8a8
Kinga <kinga.zawarty@pluckybytes.com>
2022-03-02 14:11:03 +0100

    get first avaiable

--- tic_tac_toe_ai/main_scene/computer_player.gd
+++ tic_tac_toe_ai/main_scene/computer_player.gd
@@ -2,21 +2,35 @@ extends Control
 
 
 func make_move(board : Array) -> void:
-	yield(get_tree().create_timer(1.0), "timeout")
+	yield(get_tree().create_timer(0.5), "timeout")
 	print("computer is making move")
 	
-	var blank_pieces : Array = []
+	var temp_board = copy_board(board)
+	var avaiable = get_first_avaiable(board)
+	if !avaiable : 
+		return
+	
+	
+	#pick random for now
+	#var rng = RandomNumberGenerator.new()
+	#var random_number = rng.randf_range(0, avaiable.size() - 1)
+	#var random_choice = avaiable[random_number]
+	#print("random ", random_number, random_choice)
+	avaiable._on_button_pressed_up()
+
+
+
+func get_first_avaiable(board : Array):
 	for row in board:
 		for r in row:
 			if r.status == -1:
-				blank_pieces.append(r)
+				return r
+
+
+func copy_board(board : Array) -> Array :
+	var copied_board : Array = board.duplicate(true)
+	for i in range(3):
+		for j in range(3):
+			copied_board[i][j] = board[i][j].status
 	
-	if !blank_pieces : 
-		return
-	
-	#pick random for now
-	var rng = RandomNumberGenerator.new()
-	var random_number = rng.randf_range(0, blank_pieces.size() - 1)
-	var random_choice = blank_pieces[random_number]
-	print("random ", random_number, random_choice)
-	random_choice._on_button_pressed_up()
+	return copied_board

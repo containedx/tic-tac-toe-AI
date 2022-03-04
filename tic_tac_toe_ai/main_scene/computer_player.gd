65fd3817636207a838f1091c9645e623ef046f1d
Kinga <kinga.zawarty@pluckybytes.com>
2022-03-04 15:05:57 +0100

    omg I forgot about TIE

--- tic_tac_toe_ai/main_scene/computer_player.gd
+++ tic_tac_toe_ai/main_scene/computer_player.gd
@@ -1,15 +1,19 @@
 extends Control
 
+# TESTTT
+var board0 = [[1, 1, -1], [-1, 0, -1], [-1,-1,-1]]
 
+	
 func make_move(board : Array) -> void:
 	yield(get_tree().create_timer(0.5), "timeout")
 	print("computer is making move")
 	
 	#board with tatus values only
 	var test_board = copy_board(board)
-	print(test_board)
+	print ("test board ", test_board)
+	#print(test_board)
 	
-	var best_score = -INF
+	var best_score = -10000
 	var move_coordinates = {
 		x = 0,
 		y = 0
@@ -21,15 +25,15 @@ func make_move(board : Array) -> void:
 				#try to make move there on test board
 				test_board[i][j] = 0 #because computer is o 
 				var score = minimax(test_board, 0, false)
-				print(score)
+				test_board[i][j] = -1
 				if score > best_score:
-					score = best_score
+					best_score = score
 					move_coordinates.x = i
 					move_coordinates.y = j
-					print(i," ",j)
 	
 	
 	#make move
+	print("best score" , best_score)
 	print(move_coordinates)
 	board[move_coordinates.x][move_coordinates.y]._on_button_pressed_up()
 
@@ -37,10 +41,11 @@ func make_move(board : Array) -> void:
 func minimax(board : Array, depth, is_max) -> int:
 	var result = check_win(board)
 	if result != -1 : # => someone won
+		#print("won " , result, "   ", board)
 		return count_score(result)
 	
 	if is_max : #calculating computer best move
-		var best_score = -INF
+		var best_score = -100000
 		
 		for i in range(3):
 			for j in range(3):
@@ -48,12 +53,13 @@ func minimax(board : Array, depth, is_max) -> int:
 					board[i][j] = 0 #computer ai move
 					var score = minimax(board, depth + 1, false)
 					board[i][j] = -1
-					best_score = max(score, best_score)
+					if score > best_score :
+						best_score = score
 		
 		return best_score
 	
 	else : #calculating worst senario for human
-		var worst_score = +INF
+		var worst_score = 10000
 		
 		for i in range(3):
 			for j in range(3):
@@ -61,31 +67,31 @@ func minimax(board : Array, depth, is_max) -> int:
 					board[i][j] = 1 #human move
 					var score = minimax(board, depth + 1, true)
 					board[i][j] = -1
-					worst_score = min(score, worst_score)
+					if score < worst_score :
+						worst_score = score
 		
 		return worst_score
-
-
 
 
 func count_score(result : int) -> int :
 	match result:
 		1: # x won
-			return -1
+			return -10
 		0: # o won
-			return 1
+			return 10
 	return 0
+
 
 func copy_board(board : Array) -> Array :
 	var copied_board : Array = board.duplicate(true)
 	for i in range(3):
 		for j in range(3):
 			copied_board[i][j] = board[i][j].status
-	
+			
 	return copied_board
 
 
-#return -1, 0 or 1 (none, o, x)
+#return -1, 0 or 1 (none, o, x), 2 if TIE
 func check_win(board : Array) -> int:
 	var result = -1
 	#check rows
@@ -107,5 +113,16 @@ func check_win(board : Array) -> int:
 			result = board[0][0]
 		if board[0][2] == board[1][1] && board[1][1] == board[2][0]:
 			result = board[1][1]
+	
+	# check if TIE
+	if result == -1:
+		var count_avaiable = 0
+		for row in board:
+			for r in row:
+				if r == -1:
+					count_avaiable += 1
+		if count_avaiable == 0:
+			result = 2
+	
 	
 	return result
